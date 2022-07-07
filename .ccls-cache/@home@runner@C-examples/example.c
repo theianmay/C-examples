9b1bd5d030d208f673e35c734265c70f8cdb815c
Ian M <52084309+theianmay@users.noreply.github.com>
2022-07-07 20:52:54 +0000

    Added example

---
+++ .ccls-cache/@home@runner@C-examples/example.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int example() {
+  printf("Enter your birthday month: ");
+  char bday[32];
+  fgets(bday, 32, stdin);
+  bday[strlen(bday)-1] = '\0';
+  printf("Happy Birthday in the month of %s!\n", bday);
+ return EXIT_SUCCESS;
+}
---
+++ 01_text-entry.d
@@ -0,0 +1,1 @@
+01_text-entry.o 01_text-entry.d : 01_text-entry.c
